e165c291b073c4044e41a6fd54012623b2b0f63f
jose.menjivarhernandez <jamenjivar2010@hotmail.com>
2022-01-30 12:51:59 -0700

    Functionalisty added to count and store words successfully

--- javascript/logic.js
+++ javascript/logic.js
@@ -8,18 +8,18 @@ function getStats(txt) {
     //Basic logic to count number of characters
     let chars = 0;
     let strArr = txt.split('');
+    console.log(strArr)
     strArr.forEach(function(){chars+=1;});
    
     // Basic logic to count the number of lines
     let numLines = 0;
     let linesArr = txt == "" ? [] : txt.split('\n');
-    linesArr.forEach(function(){numLines+=1;})
+    linesArr.forEach(function(){numLines+=1;});
 
     // Basic logic to count non empty lines
     let nonEmptyLines = 0;
-    for (var line of linesArr){
+    for (let line of linesArr){
         let charArr = line.split('');
-        console.log(charArr);
         for (let character of charArr){
             if(character.charCodeAt(0) > 32){
                 nonEmptyLines+=1;
@@ -28,18 +28,65 @@ function getStats(txt) {
         } 
     }
 
+    function checkMap(word, map){
+        if (typeof map.get(word) != "undefined"){
+            let value = map.get(word);
+            map.set(word, value+=1); 
+        } 
+        else{
+            map.set(word,1);
+        } 
+    }
 
+    function isNumeric(char){
+        let code = char.charCodeAt(0);
+        if(code > 47 && code < 58){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    function removePunctuation(word){
+        let punctuationRegex = /[,.?!]/g;
+        let newWord = word.replace(punctuationRegex, '');
+        return newWord;        
+    }
+
+    const wordMap = new Map();
+    let wordCount = 0;
+
+    let word = "";    
+    for(let character of strArr){
+        let code = character.charCodeAt(0);
+        if(character != ' ' && character != '\n' && (code > 32 && !isNumeric(character))){
+            word = word + character;
+        }
+        else{
+            let pWord = removePunctuation(word);
+            checkMap(pWord, wordMap);
+            wordCount +=1;
+            word = "";
+            word = isNumeric(character) ? (word + character) : word;
+        }
+    }
+    wordCount+=1;
+    let finalWord = removePunctuation(word);
+    checkMap(finalWord, wordMap);
+   
+    console.log(wordMap);
     return {
         nChars: chars,                                                     
-        nWords: 22,
+        nWords: wordCount,
         nLines: numLines,
         nNonEmptyLines: nonEmptyLines,
         averageWordLength: 3.3,
         maxLineLength: 33,
         tenLongestWords: ["xxxxxxxxx", "123444444"],
-        tenMostFrequentWords: ["a", "this", "the"]
+        tenMostFrequentWords: ["a", "this", "the"],
     };
 
 }
 
-getStats("hello\nguys");
+console.log(getStats("hello\nguys guys"));
