344d4a17cfca088ba061d9a9ed4a701a38aa30b7
jose.menjivarhernandez <jamenjivar2010@hotmail.com>
2022-02-10 00:35:51 -0700

    App commenting done and minor tweaks to website

--- javascript/logic.js
+++ javascript/logic.js
@@ -10,13 +10,15 @@ function getStats(txt) {
     let strArr = txt.split('');
     strArr.forEach(function(){chars+=1;});
    
-    // Basic logic to count the number of lines
+    // Basic logic to count the number of lines, splitting the total input by 
+    // the newline character
     let numLines = 0;
     let linesArr = txt == "" ? [] : txt.split('\n');
     console.log(linesArr);
     linesArr.forEach(function(){numLines+=1;});
 
-    // Basic logic to count non empty lines
+    // Basic logic to count non empty lines by checking if the line contains
+    //a concrete character
     let nonEmptyLines = 0;
     for (let line of linesArr){
         let charArr = line.split('');
@@ -29,6 +31,17 @@ function getStats(txt) {
     }
 
     //******************************* Utility functions **************************************/
+
+
+    /**
+     * function that takes in a word and a Map containing a word:number KV pair if it fulfills certain requirements. 
+     if word exists in the map, 1 is added to the value. If the word does not, 
+     then a new KV pair is created into the Map
+     * 
+     * @param {word to be potentially added to the map or added a value} word 
+     * @param {map to be updated} map 
+     */
+
     function checkMap(word, map){
         let char = word.charAt(0);
         if (word != "" && !isSeparator(word) && (isNumeric(char) || isAlphabetical(char))){
@@ -43,6 +56,7 @@ function getStats(txt) {
         }   
     }
 
+    // Function checks whether a given character is Numeric or not
     function isNumeric(char){
         let code = char.charCodeAt(0);
         if(code > 47 && code < 58){
@@ -52,7 +66,8 @@ function getStats(txt) {
             return false;
         }
     }
-
+    
+    // Functions checks whether a given character is Alphabetical
     function isAlphabetical(char){
         let code = char.charCodeAt(0);
         if((code >= 65 && code <= 90) || (code >= 97 && code <= 122)){
@@ -64,6 +79,8 @@ function getStats(txt) {
 
     }
 
+    // Function checks whether a certain character is one of the 
+    // separator characters outlines in the assignment description
     function isSeparator(char){
         if(char === ' ' || char === '\n'){
             return true;
@@ -73,6 +90,19 @@ function getStats(txt) {
         }
     }
 
+
+    // Compare function for the sorting required for this assignment. 
+    /**
+     * 
+     * @param {First KV Pair fom a map to be sorted } a 
+     * @param {Second KV Pair fom a map to be sorted } b 
+     * @returns a sorting integer value for the sort function
+     * 
+     * 
+     * This sorting function sorts by the Value from the KV pair first. If thats the same, it sorts by the key alphabetically, 
+     * with numbers first as per Lorans' request. If not, you would just do a[1].localCompareb[1] I believe
+     * Please dont mark me wrng if this is not what he wanted, the other way was much easier to do...
+     */
     function tieBraker(a, b){
         // Since we are assuming that both a and b are length 2 arrays with a KV pair
         if(a[1] == b[1]){
@@ -94,6 +124,9 @@ function getStats(txt) {
     }
 
 
+    /**
+     * finds the maximum length line of an array of lines.
+     */
     function findMaxLineLength(linesArr){
         let initLength = 0;
         for(let line of linesArr){
@@ -107,10 +140,19 @@ function getStats(txt) {
 
     //***************** End of Utility Function Section ***********************************************/
 
+
+
     const wordCountMap = new Map();
     let wordCount = 0;
     let word = "";
     let lastChar = "";
+
+    /**
+     * for loop in charge of splitting all the words in the text
+     * Essentially, checks every character. If not a separator character, then adds a character to the
+     * previous word if its of the same type (numeric or alphabetical) 
+     * If the character is a separator, then we just add the last word and start the loop again
+     */
 
     for(let character of strArr){
 
@@ -152,12 +194,12 @@ function getStats(txt) {
             lastChar = "";
         }
     }
-
-    // let finalWord = removePunctuation(word).toLowerCase();
     checkMap(word.toLowerCase(), wordCountMap);
-    console.log(wordCountMap)
 
-    //Starting average word length section by getting the length of every word
+
+    /**
+     * For loop that creates the wordLength map 
+     */
     const wordLengthMap = new Map();
     let keys = wordCountMap.keys();
     for (let strVar of keys){
@@ -166,12 +208,17 @@ function getStats(txt) {
     }
 
 
-    //Section of the code to get average string legth
+    
+    /**
+     *Section of the code to get average string legth
+     We use the keys of the wordLength map to access the map itself and calcualte the average string length. 
+     */
+
     let totalStringsLength = 0
     let lengthKeys = wordLengthMap.keys();
     for (let value of lengthKeys){
         //You can take out the wordCountMap.get(value) multiplication after asking the question
-        // Is the average length of a word based on the total lengthh of all words (including repetition) or not inclding repetition
+        // Is the average length of a word based on the total lengthh of all words (including repetition)
         totalStringsLength = totalStringsLength + (wordLengthMap.get(value) * wordCountMap.get(value));
     }
     let averageStringLength = (totalStringsLength/wordCount).toFixed(2);
@@ -180,14 +227,16 @@ function getStats(txt) {
 
     // Sources: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
     //          https://www.javascripttutorial.net/es6/javascript-map/
-    // Source:  https://medium.com/coding-at-dawn/how-to-sort-an-array-numerically-in-javascript-2b22710e3958
+
+    /**
+     * Sorted the maps with the required sorting function, which can easily be changed if necessaty
+     */
 
     let sortedFrequencyMap = new Map([...wordCountMap.entries()].sort(tieBraker));
     let sortedLengthMap = new Map([...wordLengthMap.entries()].sort(tieBraker));
 
-    console.log(sortedFrequencyMap);
-    console.log(sortedLengthMap);
 
+    // Essentially just creating the output arrays in the following 15 lines
     let tenLongestStrings = [];
     let tenFrequentStrings = [];
     let sortedLengthKeys = [...sortedLengthMap.keys()];
